406d8ecab57b3d24af8551ce88c3290680b61bd3
m-dahmani <med_wirari@hotmail.com>
2024-07-13 01:35:43 +0100

    Nest serializers & Add pagination

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,19 +1,45 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from shop.models import Category, Product, Article
 
 
 class CategorySerializer(ModelSerializer):
+    # define the related_name 'product' attribute by specifying a serializer set to 'many=True'
+    # because there are multiple products for a category
+    # to have more details on the products in the category do not display only the IDs
+    # products = ProductSerializer(many=True)
+
+    # Using a `SerializerMethodField', it is necessary to write a method named 'get_extra_info'
+    # where extra_info is the name of the attribute, here 'products'
+    products = SerializerMethodField()   # gives the possibility to filter the products to be returned
+
     class Meta:
         model = Category
-        # fields = ['id', 'name']
-        fields = ['id', 'name', 'date_created', 'date_updated']
-        # fields = '__all__'   # ou liste des champs que vous voulez inclure
+        # add the list of IDs in an attribute with the same name as the related_name=='products'
+        fields = ['id', 'name', 'date_created', 'date_updated', 'products']
+
+    def get_products(self, obj):
+        """Le paramètre 'obj' est l'instance de la catégorie consultée.
+        Dans le cas d'une liste, cette méthode est appelée autant de fois qu'il y a d'entités dans la liste
+        Show only products that are active"""
+        queryset = obj.products.filter(active=True)
+        # The serializer is created with the queryset defined and always set as many=True
+        serializer = ProductSerializer(queryset, many=True)
+        # the '.data' property is the rendering of our serializer that we return here
+        return serializer.data  # Calculate some data to return.
 
 
 class ProductSerializer(ModelSerializer):
+
+    articles = SerializerMethodField()  # gives the possibility to filter the products to be returned
+
     class Meta:
         model = Product
-        fields = ['id', 'name', 'date_created', 'date_updated', 'category']
+        fields = ['id', 'name', 'date_created', 'date_updated', 'category', 'articles']
+
+    def get_articles(self, instance):
+        queryset = instance.articles.filter(active=True)
+        serializer = ArticleSerializer(queryset, many=True)
+        return serializer.data  # Calculate some data to return.
 
 
 class ArticleSerializer(ModelSerializer):
--- shop/tests.py
+++ shop/tests.py
@@ -8,20 +8,72 @@ class ShopAPITestCase(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
+        """ Pour configurer les données de test qui seront partagées par tous les tests de la classe
+        En utilisant @classmethod, les données sont créées une fois pour toutes les méthodes de test,
+        ce qui permet de gagner du temps et de s'assurer que les tests démarrent avec un état de base cohérent"""
         # Let's create two categories of which only one is active
+        # Attribue l'instance de la catégorie créée à un attribut de classe appelé category.
+        # Crée une nouvelle instance de la classe Category avec le nom 'Fruits' et l'attribut active défini sur True
         cls.category = Category.objects.create(name='Fruits', active=True)
+        # Crée une nouvelle instance de la classe Category avec le nom 'Légumes' et l'attribut active défini sur False.
+        # Cette instance n'est pas stockée dans un attribut de classe car elle n'est pas nécessaire dans les tests suivants.
         Category.objects.create(name='Légumes', active=False)
 
+        #  Attribue l'instance du produit créé à un attribut de classe appelé product.
+        # Crée une nouvelle instance de la classe Product avec le nom 'Ananas' et l'attribut active défini sur True,
+        # associée à la catégorie category créée précédemment.
         cls.product = cls.category.products.create(name='Ananas', active=True)
+        # Crée une nouvelle instance de la classe Product avec le nom 'Banane' et l'attribut active défini sur False,
+        # associée à la catégorie category. Cette instance n'est pas stockée dans un attribut de classe
+        # car elle n'est pas nécessaire dans les tests suivants.
         cls.category.products.create(name='Banane', active=False)
 
+        # Attribue l'instance de la catégorie créée à un attribut de classe appelé category_2.
+        # Crée une nouvelle instance de la classe Category avec le nom 'Légumes' et l'attribut active défini sur True
         cls.category_2 = Category.objects.create(name='Légumes', active=True)
+        # Attribue l'instance du produit créé à un attribut de classe appelé product_2.
+        # Crée une nouvelle instance de la classe Product avec le nom 'Tomate' et l'attribut active défini sur True,
+        # associée à la catégorie category_2.
         cls.product_2 = cls.category_2.products.create(name='Tomate', active=True)
 
     def format_datetime(self, value):
         # Format DateTime Helper
         # a helper allowing you to format a date as a character string in the same format as that of the API
         return value.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+    def get_article_list_data(self, articles):
+        return [
+            {
+                'id': article.pk,
+                'name': article.name,
+                'date_created': self.format_datetime(article.date_created),
+                'date_updated': self.format_datetime(article.date_updated),
+                'product': article.product_id
+            } for article in articles
+        ]
+
+    def get_product_list_data(self, products):
+        return [
+            {
+                'id': product.pk,
+                'name': product.name,
+                'date_created': self.format_datetime(product.date_created),
+                'date_updated': self.format_datetime(product.date_updated),
+                'category': product.category_id,
+                'articles': self.get_article_list_data(product.articles.filter(active=True))
+            } for product in products
+        ]
+
+    def get_category_list_data(self, categories):
+        return [
+            {
+                'id': category.id,
+                'name': category.name,
+                'date_created': self.format_datetime(category.date_created),
+                'date_updated': self.format_datetime(category.date_updated),
+                'products': self.get_product_list_data(category.products.filter(active=True))
+            } for category in categories
+        ]
 
 
 class TestCategory(ShopAPITestCase):
@@ -34,16 +86,8 @@ class TestCategory(ShopAPITestCase):
         # We check that the status code is indeed 200
         self.assertEqual(response.status_code, 200)
 
-        expected = [
-            {
-                'id': category.id,
-                'name': category.name,
-                'date_created': self.format_datetime(category.date_created),
-                'date_updated': self.format_datetime(category.date_updated),
-            } for category in [self.category, self.category_2]
-        ]
         # the values returned are those expected
-        self.assertEqual(response.json(), expected)
+        self.assertEqual(response.json()['results'], self.get_category_list_data([self.category, self.category_2]))
 
     def test_create(self):
         category_count = Category.objects.count()
@@ -60,27 +104,16 @@ class TestProduct(ShopAPITestCase):
 
     url = reverse_lazy('product-list')
 
-    def get_product_detail_data(self, products):
-        return [
-            {
-                'id': product.pk,
-                'name': product.name,
-                'date_created': self.format_datetime(product.date_created),
-                'date_updated': self.format_datetime(product.date_updated),
-                'category': product.category_id
-            } for product in products
-        ]
-
     def test_list(self):
         """Vérifie que la liste des produits actifs est retournée correctement."""
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(self.get_product_detail_data([self.product, self.product_2]), response.json())
+        self.assertEqual(self.get_product_list_data([self.product, self.product_2]), response.json()['results'])
 
     def test_list_filter(self):
         response = self.client.get(self.url + '?category_id=%i' % self.category.pk)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(self.get_product_detail_data([self.product]), response.json())
+        self.assertEqual(self.get_product_list_data([self.product]), response.json()['results'])
 
     def test_create_not_allowed(self):
         """Vérifie qu'on ne peut pas créer un produit via l'API."""
